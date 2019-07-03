53738cabdd5d30d021b28b4c6594e12c230f9eba
hamr34@gmail.com <Hamrit2ham>
2019-07-03 22:13:22 +0100

    §§§HHG

--- app/src/main/java/com/example/appname/View/sort/SortActivity.java
+++ app/src/main/java/com/example/appname/View/sort/SortActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.appname.Model.Image;
 import com.example.appname.Model.LoadUnsortedImagesTask;
